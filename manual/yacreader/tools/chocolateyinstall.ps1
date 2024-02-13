7269b7b76be20527ea61da5ba31f2efd2786175d
Daniel Markiewicz <daniel.markiewicz92@gmail.com>
2024-02-13 13:54:03 -0500

    Removed unnecessary scripts. Updated YACReader to v1.14

--- manual/yacreader/tools/chocolateyinstall.ps1
+++ manual/yacreader/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'YACReader*winx86.exe'
 $file64Location = Join-Path $toolsDir 'YACReader*winx64.exe'
-$url        = 'https://github.com/YACReader/yacreader/releases/download/9.13.1/YACReader-v9.13.1.2307166-winx86-7z.exe'
-$url64      = 'https://github.com/YACReader/yacreader/releases/download/9.13.1/YACReader-v9.13.1.2307166-winx64-7z-qt6.exe'
+$url        = 'https://github.com/YACReader/yacreader/releases/download/9.14.1/YACReader-v9.14.1.2402053-winx86-7z.exe'
+$url64      = 'https://github.com/YACReader/yacreader/releases/download/9.14.1/YACReader-v9.14.1.2402053-winx64-7z-qt6.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -13,9 +13,9 @@ $packageArgs = @{
   file			= $fileLocation
   file64		= $file64Location
   softwareName  = 'YACReader*'
-  checksum      = '2d951763c7dc9447fd64b02c565a2445fd38eab63913b7615557a2a8312b2c8e'
+  checksum      = 'a0998e1c7d871e4f88e7b81c222673cbd687c09d897c67a0f75f273a2a860433'
   checksumType  = 'sha256'
-  checksum64    = '80aef50be7cb00d2f2787aa5d1f2fb47242a06f406e5d6d6df34908c9c83a15a'
+  checksum64    = 'a6efe0a7b9935b309a08fc7ec9129d5da8004d990f5c6d4ce9628ffce80a6254'
   checksumType64= 'sha256'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0, 3010, 1641)
