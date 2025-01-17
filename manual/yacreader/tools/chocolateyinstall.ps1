71a8b25c890106f66c2b7686e0d90bd813c9d88c
Daniel Markiewicz <daniel.markiewicz92@gmail.com>
2025-01-16 21:02:34 -0500

    Updated x86-64 source as original version could not find suitable QT plugin and would not run.

--- manual/yacreader/tools/chocolateyinstall.ps1
+++ manual/yacreader/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 $fileLocation = Join-Path $toolsDir 'YACReader*winx86.exe'
 $file64Location = Join-Path $toolsDir 'YACReader*winx64.exe'
 $url        = 'https://github.com/YACReader/yacreader/releases/download/9.15.0/YACReader-v9.15.0.2501014-winx86-7z.exe'
-$url64      = 'https://github.com/YACReader/yacreader/releases/download/9.15.0/YACReader-v9.15.0.2501014-winx64-7z.exe'
+$url64      = 'https://github.com/YACReader/yacreader/releases/download/9.15.0/YACReader-v9.15.0.2501014-winx64-7z-qt6.exe'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -15,7 +15,7 @@ $packageArgs = @{
   softwareName  = 'YACReader*'
   checksum      = '5b1ca85223415e6a01f826a7a9ca1242ebe0a691a982a35179883a8926059ae1'
   checksumType  = 'sha256'
-  checksum64    = 'b0d9aaac74290a4811c1f8f31b917314c9ae0e1b41c4e72c2b6b228dd4d713e6'
+  checksum64    = 'eb7f96d550e1bccbeca9a8f52af0dabd1215810dc6232f9883d7e955892a5276'
   checksumType64= 'sha256'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0, 3010, 1641)
